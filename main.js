bd69b0c7c8966be2754f63d5ecd2d696b21e5037
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-03-30 15:43:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,133 @@
+// 1-misol
+
+// let promt = prompt("...Enter your number")
+// let toq = 0
+// let juft = 0
+// for (let i = 0; i <= promt; i++) {
+//     if (i % 2 == 0) {
+//         juft += i
+//     }else{
+//         toq += i
+//     }
+// }
+// let result = Math.abs(juft - toq)
+// console.log(`juft va toq sonlar yigindisi farqi ${result}ga  teng`)
+
+
+
+
+
+// 2-misol
+
+// let prompt1 = prompt("...Enter your number")
+// let prompt2 = prompt("...Enter your number")
+// if (prompt1 < prompt2) {
+//     for (let i = prompt1; i <= prompt2; i++) {
+//         console.log(i);
+//     }
+// }else{
+//     console.log("1-kiritgan soniz 2-songa nisbatan kotta bolishi kerak")
+// }
+
+
+
+
+// 3-misol
+
+// let prompt_qiymat = +prompt("Enter your number ...") 
+// let total = 0
+// for (let i = 1; i <= prompt_qiymat; i++) {
+//     if (i % 2 == 0){
+//         total ++
+//     }
+// }
+// console.log(total);
+
+
+
+
+// 4-misol
+
+// let str= "Hello"
+// let count = 0
+// for (let i = 0; i< str.length; i++) {
+//     count++
+// }
+// console.log(count);
+
+
+
+
+
+// 5-misol
+
+// let prompt_qiymat = +prompt("Enter your number ...")
+// let total = 0
+// for(let i = 1; i <= prompt_qiymat; i++ ) {
+//     if (prompt_qiymat % i == 0) {
+//         total ++
+//     }
+// }
+// if (total == 2) {
+//     console.log("bu tub son");
+// }
+// else {
+
+//     console.log("bu tub son emas");
+// }
+
+
+
+
+
+// 6-misol
+
+// let str prompt = ("Enter your text")
+// let count = 0
+// for (let i = 0; i < str.length; i++){
+//     let l = str[i]
+//     if (l === "a" || l === "o" || l === "i" || l === "u" || l === "e") {
+//         count ++
+//     }
+// }
+// console.log(count)
+
+
+
+
+
+// 7-task
+
+// let text = prompt("Enter your text ...") 
+// let total = 0
+// let count = 0
+// for (let i = 0; i < text.length; i++) {
+//     count ++
+//     let b = text[i]
+//     if(b == "a" || b == "o" || b == "i" || b == "u" || b == "e") {
+//         total ++
+//     }
+// }
+// console.log(count - total);
+
+
+
+
+
+
+
+// 8-task
+
+// let prompt_number = +prompt("Enter your number")
+// let total = 0
+// for (let i = 0; i < prompt_number; i++) {
+//     if (prompt_number % i == 0) {
+//         total +=i
+//     }
+// }
+// if (prompt_number == total) {
+//     console.log("mukammal son");
+// }
+// else {
+//     console.log("mukammal son emas");
+// }
